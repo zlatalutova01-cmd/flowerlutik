38a049185e25cd7406600960903bcbca4e619582
zlatalutova01-cmd <zlatalutova01@gmail.com>
2026-04-21 21:30:21 +0400

    Add files via upload

---
+++ html/script.js
@@ -0,0 +1,87 @@
+const blocks = document.querySelectorAll(
+  ".card, .feature, .review, .form-box"
+);
+
+function showOnScroll() {
+  const triggerBottom = window.innerHeight * 0.85;
+
+  blocks.forEach(el => {
+    const boxTop = el.getBoundingClientRect().top;
+
+    if (boxTop < triggerBottom) {
+      el.classList.add("show");
+    }
+  });
+}
+
+window.addEventListener("scroll", showOnScroll);
+showOnScroll();
+
+function toggleMenu() {
+    const menu = document.getElementById("dropdown");
+
+    if (menu.style.display === "flex") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "flex";
+    }
+}
+
+function openModal(title, desc, img, price, country) {
+    document.getElementById("modal").style.display = "flex";
+
+    document.getElementById("modal-title").innerText = title;
+    document.getElementById("modal-desc").innerText = desc;
+    document.getElementById("modal-img").src = img;
+
+    document.getElementById("modal-price").innerText = price;
+    document.getElementById("modal-country").innerText = "Страна: " + country;
+}
+
+function closeModal() {
+    document.getElementById("modal").style.display = "none";
+}
+
+function openOrder() {
+    document.getElementById("order").style.display = "block";
+}
+
+
+function openTG() {
+    window.open("https://t.me/LutovAleksander", "_blank");
+}
+
+
+function sendOrder() {
+    let phone = document.getElementById("phone").value;
+
+    if (phone === "") {
+        alert("Введите номер!");
+        return;
+    }
+
+    let token = "8727744618:AAFVmmGWcoF7Ywqr9tiijPDGiKcy6UNTGa8"; // ОБЯЗАТЕЛЬНО поменяй!
+    let chat_id = "6091253525";
+
+    let message = "📩 Новая заявка с сайта!\n\n📞 Телефон: " + phone;
+
+    fetch("https://api.telegram.org/bot" + token + "/sendMessage", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            chat_id: chat_id,
+            text: message
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        alert("Заявка отправлена!");
+    })
+    .catch(err => {
+        console.log(err);
+        alert("Ошибка отправки");
+    });
+}
